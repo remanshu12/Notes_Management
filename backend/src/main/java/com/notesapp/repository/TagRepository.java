2d0e05e95e0645dcd18b84a55343cb95c400ef70
remanshu12 <remanshugoyal1@gmail.com>
2026-05-05 15:14:07 +0530

    add TagRepository for tag management

---
+++ backend/src/main/java/com/notesapp/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.notesapp.repository;
+
+import com.notesapp.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByNameIgnoreCase(String name);
+}
