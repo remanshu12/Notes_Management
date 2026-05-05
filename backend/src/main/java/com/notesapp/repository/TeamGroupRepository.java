646700768a381316dffe4fbddef2a2fc1ab88457
remanshu12 <remanshugoyal1@gmail.com>
2026-05-05 15:14:35 +0530

    add TeamGroupRepository for group collaboration

---
+++ backend/src/main/java/com/notesapp/repository/TeamGroupRepository.java
@@ -0,0 +1,7 @@
+package com.notesapp.repository;
+
+import com.notesapp.entity.TeamGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamGroupRepository extends JpaRepository<TeamGroup, Long> {
+}
